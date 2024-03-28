0e0f304b06ca7c79cbe58564c081fda9ad46e8fd
Aayush Vyas <aayush25vyas@gmail.com>
2024-03-28 17:44:27 +0530

    Console.log in js and hello world first prgram injs

---
+++ 3. Javascript - Giving Dynamic nature/1. Introduction to Javascript/First program in JS/script.js
@@ -0,0 +1,2 @@
+console.log("Hello World");
+//! To print anything on console screen or to take output in javascript we use the console.log function to print output in above js code we write printing function console.log and round bracket and inside it we write 'Hello World' in single inverted commas which was called String so the Hello World was print on the console.
